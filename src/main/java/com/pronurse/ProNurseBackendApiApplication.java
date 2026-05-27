34c444c95f4f4d58cb367ea1e97fef21d22dcb98
shawbirendra <krs.birendra@gmail.com>
2026-05-28 00:29:46 +0530

    initial project setup commit

---
+++ src/main/java/com/pronurse/ProNurseBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.pronurse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProNurseBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProNurseBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pronurse/ProNurseBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pronurse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProNurseBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
